e2aae1255ecd849762b63a9be276d534175c9f9d
Colin Gray <colin.gray@equalexperts.com>
2026-04-14 09:47:38 +0100

    Database schema and API update for create new project

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,12 +1,12 @@
 import { health } from '../routes/health.js'
 import { dbInfo } from '../routes/db-info.js'
-import { getProjects, getProject } from '../routes/projects.js'
+import { getProjects, getProject, createProject } from '../routes/projects.js'
 
 const router = {
   plugin: {
     name: 'router',
     register: (server, _options) => {
-      server.route([health, dbInfo, getProjects, getProject])
+      server.route([health, dbInfo, getProjects, getProject, createProject])
     }
   }
 }
